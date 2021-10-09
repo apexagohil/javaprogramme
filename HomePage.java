36d13172ccda2fecceebdd928f3098fe51d36592
N <Gohil@COFLEIC081LT.cofresh.local>
2021-10-09 12:53:15 +0100

    this is my first java files

---
+++ HomePage.java
@@ -0,0 +1,20 @@
+package demo.nopcommerce;
+
+public class HomePage {
+
+    //Main method to execute the program
+    public static void main(String[] args) {
+        System.out.println("My First Java program");
+        System.out.println("Git learning");
+        
+        //print statement to print some text
+
+
+
+
+    }
+
+
+}
+
+
---
+++ LoginPage.java
@@ -0,0 +1,5 @@
+package demo.nopcommerce;
+
+public class LoginPage {
+
+}
